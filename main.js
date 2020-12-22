c2861178bc94b659717584ad56f655eb47624210
edward <edhuosb18mail.com>
2020-12-22 09:59:45 -0500

    mi primer commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+let b=document.getElementById("mostrar");
+let m=document.getElementById("vitto");
+
+
+// let f=()=>{
+// m.classList.toggle("salir");
+// // document.body.classList.toggle("black");
+
+
+// };
+
+
+// b.addEventListener("click",f);
+
+
+b.addEventListener("click",(e)=>{
+
+
+// e.preventDefault;
+m.style.filter="opacity(1)";
+
+
+})
